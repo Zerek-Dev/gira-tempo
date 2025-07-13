f199776661b60a7c3793d1ac0f0dd6178575259e
Felippe Costa <58724710+Zerek-Dev@users.noreply.github.com>
2025-07-13 23:40:12 +0100

    1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
-import Catalog from "./pages/Catalog";
-import AdminPanel from "./pages/AdminPanel";
-import Navbar from "./components/Navbar";
 
+import React from 'react';
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/catalog" element={<Catalog />} />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </Router>
-  );
+  return <h1>Gira Tempo Online</h1>;
 }
-
 export default App;
